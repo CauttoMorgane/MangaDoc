26a30bd5b3a980598d39f98db23217b535381a96
morganecautto <morganecautto@gmail.com>
2019-06-17 15:43:01 +0200

    front

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,14 +2,6 @@
 
 namespace App\Controller;
 
-//use App\Entity\Article;
-//use App\Form\ArticleType;
-//use App\Repository\ArticleRepository;
-//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -51,10 +52,11 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}", name="user_show", methods={"GET"})
      */
-    public function show(User $user): Response
+    public function show(User $user, ArticleRepository $articleRepository): Response
     {
         return $this->render('user/show.html.twig', [
             'user' => $user,
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 
