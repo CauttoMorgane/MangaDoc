ae207f6717463ff6584f5890ed7e8dfaf7893226
morganecautto <morganecautto@gmail.com>
2019-06-17 10:37:03 +0200

    crud crée

---
+++ src/Controller/ArticleControllerrrrrr.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\Routing\Annotation\Route;
+//use App\Repository\ArticleRepository;
+//
+//use App\Entity\Article;
+//use App\Form\ArticleType;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+
+class ArticleControllerrrrrr extends AbstractController
+{
+//    /**
+//     * @Route("/article", name="article.index")
+//     */
+//    public function index(ArticleRepository $articleRepository)
+//    {
+//        $articles = $articleRepository->findAll();
+//        return $this->render("article.index.html.twig", [
+//            'articles' => $articles
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/article/{id}", name="article.show")
+//     */
+//    public function show(ArticleRepository $articleRepository, int $id)
+//    {
+//        $article = $articleRepository->find($id);
+//        $articles = $articleRepository->findAll();
+//        return $this->render("article.show.html.twig", [
+//            'article' => $article,
+//            'articles' => $articles
+//        ]);
+//    }
+}
