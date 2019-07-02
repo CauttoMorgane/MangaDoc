197e6a2e7e54afa113d256ba415d92c052616683
morganecautto <morganecautto@gmail.com>
2019-07-02 11:16:47 +0200

    add choise toi different form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,26 +4,31 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password', PasswordType::class)
+//            ->add('roles')
+            ->add('gender', ChoiceType::class,[
+                'choices' => [
+                    'Male' => 'm',
+                    'Female' => 'f',
+                ]
+            ])
             ->add('firstname')
             ->add('lastname')
+            ->add('nickname')
             ->add('city')
             ->add('country')
-            ->add('gender')
             ->add('payment')
-            ->add('nickname')
+            ->add('email')
+            ->add('password', PasswordType::class)
         ;
     }
 
